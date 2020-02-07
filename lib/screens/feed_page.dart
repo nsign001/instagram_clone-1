8dd59a1b5a44b7e0d75c05121404ebf6dc9a16f5
hacyeal <hacyeal@naver.com>
2020-02-07 18:26:50 +0900

    App bar & Material color 지정 사용
    primarySwatch

--- lib/screens/feed_page.dart
+++ lib/screens/feed_page.dart
@@ -5,19 +5,23 @@ class FeedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('instagram'),
+        leading:  IconButton(onPressed: null, icon: ImageIcon(AssetImage('assets/actionbar_camera.png'), color: Colors.black)),
+        title: Image.asset('assets/insta_text_logo.png', height: 30,),
         actions: <Widget>[
-          ImageIcon(AssetImage('assets/actionbar_camera.png')),
-          ImageIcon(AssetImage('assets/direct_message.png')),
+          IconButton(
+              onPressed: null,
+              icon: ImageIcon(AssetImage('assets/actionbar_camera.png'), color: Colors.black)),
+          IconButton(
+              onPressed: null,
+              icon: ImageIcon(AssetImage('assets/direct_message.png'), color: Colors.black))
         ],
       ),
       body: ListView.builder(
           itemCount: 15,
           itemBuilder: (BuildContext context, int index) {
             return Container(
-              height: 300,
-              color: Colors.primaries[index % Colors.primaries.length]
-            );
+                height: 300,
+                color: Colors.primaries[index % Colors.primaries.length]);
           }),
     );
   }
