849be439ac18813be1b3658015f66257701652da
hacyeal <hacyeal@naver.com>
2020-02-10 17:29:02 +0900

    Feed Comment 위젯

--- lib/screens/feed_page.dart
+++ lib/screens/feed_page.dart
@@ -2,21 +2,30 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/constants/size.dart';
 import 'package:instagram_clone/utils/profile_image_path.dart';
+import 'package:instagram_clone/widgets/comment.dart';
 
 class FeedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading:  IconButton(onPressed: null, icon: ImageIcon(AssetImage('assets/actionbar_camera.png'), color: Colors.black)),
-        title: Image.asset('assets/insta_text_logo.png', height: 30,),
+        leading: IconButton(
+            onPressed: null,
+            icon: ImageIcon(AssetImage('assets/actionbar_camera.png'),
+                color: Colors.black)),
+        title: Image.asset(
+          'assets/insta_text_logo.png',
+          height: 30,
+        ),
         actions: <Widget>[
           IconButton(
               onPressed: null,
-              icon: ImageIcon(AssetImage('assets/actionbar_camera.png'), color: Colors.black)),
+              icon: ImageIcon(AssetImage('assets/actionbar_camera.png'),
+                  color: Colors.black)),
           IconButton(
               onPressed: null,
-              icon: ImageIcon(AssetImage('assets/direct_message.png'), color: Colors.black))
+              icon: ImageIcon(AssetImage('assets/direct_message.png'),
+                  color: Colors.black))
         ],
       ),
       body: ListView.builder(
@@ -30,118 +39,109 @@ class FeedPage extends StatelessWidget {
   Column _postItem(int index, BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              _postHeader('username $index'),
-              _postImage(index),
-              _postActions(),
-              _postLikes(),
-              _postCaption(context, index)
-            ],
-          );
+      children: <Widget>[
+        _postHeader('username $index'),
+        _postImage(index),
+        _postActions(),
+        _postLikes(),
+        _postCaption(context, index),
+        _allComments()
+      ],
+    );
+  }
+
+  FlatButton _allComments() {
+    return FlatButton(
+        onPressed: null,
+        child: Text(
+          'show all 18 comments',
+          style: TextStyle(color: Colors.grey[600]),
+        ));
   }
 
   Padding _postCaption(BuildContext context, int index) {
     return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: common_gap),
-              child: RichText(
-                text: TextSpan(
-                  style: DefaultTextStyle.of(context).style,
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'username $index',
-                      style: TextStyle(fontWeight: FontWeight.bold)
-                    ),
-                    TextSpan(
-                      text: ' '
-                    ),
-                    TextSpan(
-                      text: 'I am missing summer soooooooooooooo~~~~~~~~~~~much~~'
-                    ),
-                  ]
-                )
-              ),
-            );
+        padding: const EdgeInsets.symmetric(
+            horizontal: common_gap, vertical: common_xs_gap),
+        child: Comment(
+            username: 'username $index',
+            caption: 'I love usmmer soooooooooooooo much~~~~~~~~~~~!!!! '));
   }
 
   Padding _postLikes() {
     return Padding(
-              padding: const EdgeInsets.only(left: common_gap),
-              child: Text('80 likes', style: TextStyle(fontWeight: FontWeight.bold)),
-            );
+      padding: const EdgeInsets.only(left: common_gap),
+      child: Text('80 likes', style: TextStyle(fontWeight: FontWeight.bold)),
+    );
   }
 
   Row _postActions() {
     return Row(
-              children: <Widget>[
-                IconButton(
-                  icon: ImageIcon(
-                    AssetImage('assets/bookmark.png'),
-                    color: Colors.black87,
-                  ),
-                  onPressed: null,
-                ),
-                IconButton(
-                  icon: ImageIcon(
-                    AssetImage('assets/comment.png'),
-                    color: Colors.black87,
-                  ),
-                  onPressed: null,
-                ),
-                IconButton(
-                  icon: ImageIcon(
-                    AssetImage('assets/direct_message.png'),
-                    color: Colors.black87,
-                  ),
-                  onPressed: null,
-                ),
-                Spacer(),
-                IconButton(
-                  icon: ImageIcon(
-                    AssetImage('assets/heart_selected.png'),
-                    color: Colors.black87,
-                  ),
-                  onPressed: null,
-                ),
-              ],
-            );
+      children: <Widget>[
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/bookmark.png'),
+            color: Colors.black87,
+          ),
+          onPressed: null,
+        ),
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/comment.png'),
+            color: Colors.black87,
+          ),
+          onPressed: null,
+        ),
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/direct_message.png'),
+            color: Colors.black87,
+          ),
+          onPressed: null,
+        ),
+        Spacer(),
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/heart_selected.png'),
+            color: Colors.redAccent,
+          ),
+          onPressed: null,
+        ),
+      ],
+    );
   }
 
   Row _postHeader(String username) {
     return Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(14),
-                  child: CircleAvatar(
-                    backgroundImage: CachedNetworkImageProvider(
-                        getProfileImgPath(username)
-                    ),
-                    radius: profile_radius,
-                  ),
-                ),
-                Expanded(child: Text(username)),
-                IconButton(
-                  icon: Icon(Icons.more_horiz, color: Colors.black87),
-                  onPressed: null,
-                )
-              ],
-            );
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(14),
+          child: CircleAvatar(
+            backgroundImage:
+                CachedNetworkImageProvider(getProfileImgPath(username)),
+            radius: profile_radius,
+          ),
+        ),
+        Expanded(child: Text(username)),
+        IconButton(
+          icon: Icon(Icons.more_horiz, color: Colors.black87),
+          onPressed: null,
+        )
+      ],
+    );
   }
 
   CachedNetworkImage _postImage(int index) {
     return CachedNetworkImage(
-                  imageUrl: 'https://picsum.photos/id/$index/200/200',
-                  imageBuilder: (BuildContext context, ImageProvider imageProvider) =>
-                      AspectRatio(
-                        aspectRatio: 1,
-                        child: Container(
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: imageProvider,
-                                  fit: BoxFit.cover
-                                )
-                            ),
-                        ),
-                      ),
-              );
+      imageUrl: 'https://picsum.photos/id/$index/200/200',
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) =>
+          AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+        ),
+      ),
+    );
   }
 }
