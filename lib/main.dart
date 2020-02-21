a4dd944058d737d6974e79a9027db8df73571d9c
hacyeal <hacyeal@naver.com>
2020-02-21 16:58:51 +0900

    로그인 확인 후, 화면보이기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/constants/material_white_color.dart';
 import 'package:instagram_clone/main_page.dart';
+import 'package:instagram_clone/screens/auth_page.dart';
 
 void main(){
   return runApp(MyApp());
@@ -10,7 +12,15 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: StreamBuilder<FirebaseUser>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if(snapshot.hasData) {
+            return MainPage();
+          }
+          return AuthPage();
+        }
+      ),
       theme: ThemeData(
           primarySwatch: white
       )
