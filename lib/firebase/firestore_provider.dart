d523e7ce1ba5d300a08149684396899b4e620a73
hacyeal <hacyeal@naver.com>
2020-03-08 23:21:13 +0900

    User Document 입력, Data 가져오기

--- lib/firebase/firestore_provider.dart
+++ lib/firebase/firestore_provider.dart
@@ -1,22 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_clone/constants/firebase_keys.dart';
+import 'package:instagram_clone/data/user.dart';
+import 'package:instagram_clone/firebase/transformer.dart';
 
-class FirestoreProvider{
-  Future<void> sendData(){
+class FirestoreProvider with Transformer{
+
+  final Firestore _firestore = Firestore.instance;
+
+  Future<void> attemptCreateUser({String userKey, String email}) {
+    final DocumentReference userRef = _firestore.collection(COLLECTION_USERS).document(userKey);
+
+    return _firestore.runTransaction((Transaction tx) async {
+      DocumentSnapshot snapshot = await tx.get(userRef);
+      if (snapshot.exists) {
+        return;
+      } else {
+        await tx.set(userRef, User.getMapForCreateUser(email));
+      }
+    });
+  }
+
+  Stream<User> connectMyUserData(String userKey){
+    return _firestore
+        .collection(COLLECTION_USERS)
+        .document(userKey)
+        .snapshots()
+        .transform(toUser);
+  }
+
+  /*Future<void> sendData(){
     return Firestore.instance
-        .collection('Users')
+        .collection(COLLECTION_USERS)
         .document('123123123')
         .setData({'email': 'hacyeal@naver.com', 'author': 'author'});
   }
 
   Stream<dynamic> getData(){
     Firestore.instance
-        .collection('Users')
+        .collection(COLLECTION_USERS)
         .document('123123123')
         .get()
         .then((DocumentSnapshot ds){
           print(ds.data);
          });
-  }
+  }*/
 }
 
 FirestoreProvider firestoreProvider = FirestoreProvider();
